6329bcd4f83a157919c76b3c1ac46533de25cb08
Shohei Otsuka <s.otsuka@ShoheinoMacBook-Pro.local>
2018-06-16 23:08:09 +0900

    creat line bot

--- src/main/java/com/otsukatsuka/daisukebot/DaisukebotApplication.java
+++ src/main/java/com/otsukatsuka/daisukebot/DaisukebotApplication.java
@@ -1,19 +1,37 @@
 package com.otsukatsuka.daisukebot;
 
+import com.linecorp.bot.client.LineMessagingClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.linecorp.bot.model.event.Event;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
+
 
 @SpringBootApplication
 @RestController
 public class DaisukebotApplication {
 
-	@RequestMapping("/")
-	String Hello(){
-		return "Hello";
-	}
 	public static void main(String[] args) {
 		SpringApplication.run(DaisukebotApplication.class, args);
 	}
+
+    @Autowired
+    private LineMessagingClient lineMessagingClient;
+
+    @EventMapping
+    public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
+        System.out.println("event: " + event);
+        return new TextMessage(event.getMessage().getText());
+    }
+
+    @EventMapping
+    public void handleDefaultMessageEvent(Event event) {
+        System.out.println("event: " + event);
+    }
 }
