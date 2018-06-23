3b1956b7ebfebbbc0c1185d09b5a0a87e476c837
Shohei Otsuka <s.otsuka@ShoheinoMacBook-Pro.local>
2018-06-23 18:21:02 +0900

    botIdを判別してみる

--- src/main/java/com/otsukatsuka/daisukebot/repository/BotRepository.java
+++ src/main/java/com/otsukatsuka/daisukebot/repository/BotRepository.java
@@ -2,7 +2,9 @@ package com.otsukatsuka.daisukebot.repository;
 import com.otsukatsuka.daisukebot.entity.Bot;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BotRepository extends JpaRepository<Bot,Integer>, JpaSpecificationExecutor<Bot> {
 
 }
