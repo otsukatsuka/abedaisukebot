5234108c4c6e5d4fb965eee34885466247a4c984
Shohei Otsuka <s.otsuka@ShoheinoMacBook-Pro.local>
2018-06-24 18:33:11 +0900

    [delete] componetも消す

--- src/main/java/com/otsukatsuka/daisukebot/config/ConfigReader.java
+++ src/main/java/com/otsukatsuka/daisukebot/config/ConfigReader.java
@@ -1,9 +1,7 @@
 package com.otsukatsuka.daisukebot.config;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
 
-@Component
 public class ConfigReader {
 
     @Value("${settings.api.gnavi-api-key}")
