7911ded0f9d37d6a8ff35c4a798bff4b2b55e597
Shohei Otsuka <s.otsuka@ShoheinoMacBook-Pro.local>
2018-06-24 17:55:46 +0900

    [update] 結果取得処理追加

--- src/main/java/com/otsukatsuka/daisukebot/api/GAreaSmallSearchApi.java
+++ src/main/java/com/otsukatsuka/daisukebot/api/GAreaSmallSearchApi.java
@@ -2,7 +2,12 @@ package com.otsukatsuka.daisukebot.api;
 
 import com.otsukatsuka.daisukebot.Consts;
 import com.otsukatsuka.daisukebot.Enums.GnaviApiFormatType;
+import okhttp3.Call;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
 
 public class GAreaSmallSearchApi {
 
@@ -32,5 +37,26 @@ public class GAreaSmallSearchApi {
                 .setFormatType(GnaviApiFormatType.Json.getFormatType())
                 .build()
                 .buildUrl();
+    }
+
+    public String getJson() throws IOException {
+        return getResult(getUrlFormatJson());
+    }
+
+    public String getXml() throws IOException {
+        return getResult(getUrlFromatXml());
+    }
+
+    private String getResult(String url) throws IOException {
+        System.out.println("url: " + url);
+
+        Request request = new Request.Builder().url(url).build();
+        Call call = httpClient.newCall(request);
+        Response response = call.execute();
+        String result = response.body().string();
+
+        System.out.println("result: " + result.substring(0,10));
+
+        return result;
     }
 }
